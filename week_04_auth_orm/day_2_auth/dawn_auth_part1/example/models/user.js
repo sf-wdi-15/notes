10d7629e5ad14600ecbeefef8675cdde0222fe17
Michael Desa <mjdesa@gmail.com>
2014-12-09 07:05:12 -0800

    Update messages

--- week_04_auth_orm/day_2_auth/dawn_auth_part1/example/models/user.js
+++ week_04_auth_orm/day_2_auth/dawn_auth_part1/example/models/user.js
@@ -20,56 +20,56 @@ var salt = bcrypt.genSaltSync(10);
 
 
 module.exports = function (sequelize, DataTypes){
-   var User = sequelize.define('user', {
-     username: { 
-        type: DataTypes.STRING, 
-        unique: true, 
-        validate: {
-          len: [6, 30],
-          }
-    },
-    password: {
-        type:DataTypes.STRING,
-        validate: {
-          notEmpty: true
-        }
+  var User = sequelize.define('user', {
+    emails: { 
+      type: DataTypes.STRING, 
+      unique: true, 
+      validate: {
+        len: [6, 30],
       }
     },
+    password_digest: {
+      type:DataTypes.STRING,
+      validate: {
+        notEmpty: true
+      }
+    }
+  },
 
   {
     classMethods: {
       encryptPass: function(password) {
         var hash = bcrypt.hashSync(password, salt);
         return hash;
-    }, 
-      comparePass: function(userpass, dbpass) {
-      // don't salt twice when you compare....watch out for this
-        return bcrypt.compareSync(userpass, dbpass);  
-    },
-      createNewUser:function(username, password, err, success ) {
+      }, 
+      comparePass: function(email, dbpass) {
+        // don't salt twice when you compare....watch out for this
+        return bcrypt.compareSync(email, dbpass);  
+      },
+      createNewUser:function(email, password, err, success ) {
         if(password.length < 6) {
           err({message: "Password should be more than six characters"});
         }
         else{
-        User.create({
-            username: username,
-            password: User.encryptPass(password)
+          User.create({
+            email: email,
+            password_digest: User.encryptPass(password)
           }).error(function(error) {
             console.log(error);
-            if(error.username){
-              err({message: 'Your username should be at least 6 characters long', username: username});
+            if(error.email){
+              err({message: 'Your username should be at least 6 characters long', email: email});
             }
             else{
-              err({message: 'An account with that username already exists', username: username});
-              }
+              err({message: 'An account with that username already exists', email: email});
+            }
           }).success(function(user) {
             success({message: 'Account created, please log in now'});
           });
         }
       },
-      authorize: function(username, password, err, success) {
-     // find a user in the DB
-      User.find({
+      authorize: function(email, password, err, success) {
+        // find a user in the DB
+        User.find({
           where: {
             username: username
           }
@@ -83,7 +83,7 @@ module.exports = function (sequelize, DataTypes){
           else if (user === null){
             err({message: "Username does not exist"});
           }
-          else if ((User.comparePass(password, user.password)) === true){
+          else if ((User.comparePass(password, user.password_digest)) === true){
             success();
           }
           else {
@@ -91,11 +91,11 @@ module.exports = function (sequelize, DataTypes){
           }
         });
       }  
-    
-      } // close classMethods
-    } //close classMethods outer 
 
-  ); // close define user
-  return User;
+    } // close classMethods
+  } //close classMethods outer 
+
+                             ); // close define user
+                             return User;
 }; // close User function
 
